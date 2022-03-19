6c3b78b0f2fa1cc016ee55b2dc0986c92b05530f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-19 19:03:56 +0600

    data passed to product component

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Product = () => {
+const Product = (props) => {
+    const {image, title, price, description} = props.Product;
     return (
         <div>
             
