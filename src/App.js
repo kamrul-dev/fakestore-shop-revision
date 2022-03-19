fcbfcafa7028164158386af05bfc8329c8d4ced1
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-19 19:41:27 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Products from './Components/Products/Products';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Products></Products>
     </div>
   );
